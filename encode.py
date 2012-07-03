1d6f6aa7024d2fdba3f7c352b0353c82e4da97f6
David Huie <dahuie@gmail.com>
2012-07-02 23:13:25 -0700

    crack this

---
+++ encode.py
@@ -0,0 +1,21 @@
+import base64
+from Crypto.Cipher import AES
+from sys import stdin
+
+"""
+encode.py
+By: David Huie
+
+bother_encoded.py was generated as follows:
+   cat bother.py | python encode.py > bother_encoded.py
+
+secret_key below was created by taking the sha512 sum of a two character string.
+If you can't decode bother_encoded.py, I don't know even know what to say...
+"""
+program_text = stdin.read()
+
+secret_key = 'you will have to guess the key!'[:16]
+cipher = AES.new(secret_key, AES.MODE_ECB)
+encoded = base64.b64encode(cipher.encrypt(program_text + ' ' *  (16 - len(program_text) % 16)))
+
+print encoded
